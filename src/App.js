efbfe8955a13d3ec3034be71456ba9fe03f7cfd6
linda_forlizzi <la.for.lizzi@gmail.com>
2021-11-30 08:41:21 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './services/data'
+
+export default function App() {
+  	const cards = data.map(item => {
+    	return (
+		 	<Card 
+				key={item.id}
+				img={item.coverImg}
+				// description={item.description}
+				rating={item.stats.rating}
+				reviewCount={item.stats.reviewCount}
+				location={item.location}
+				title={item.title}
+				price={item.price}
+				openSpots={item.openSpots}
+			/>
+		)
+	})
+
+	return (
+		<div>
+			{/* <Navbar />
+			<Hero /> */}
+			<section className='cards-list'>
+				{cards}
+			</section>
+		</div>
+	)
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src='./assets/airbnb-logo.png' className='nav--logo' alt='logo' />
+        </nav>
+    )
+}
